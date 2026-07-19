b0c9f22989f6fae5a9639e1b84dcb54a842add23
Mahmoud1ssa <mahmoud.businessacc@gmail.com>
2026-07-20 01:35:24 +0300

    just added |Main Form Picture| as a resource to the Resources.resx & the compiler made the folder Resources

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Pizza_Make_Order_Project
 {
-    partial class Form1
+    partial class frmMainMenu
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // frmMainMenu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "frmMainMenu";
+            Text = "Pizza Vampires";
+            ResumeLayout(false);
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,8 @@
 namespace Pizza_Make_Order_Project
 {
-    public partial class Form1 : Form
+    public partial class frmMainMenu : Form
     {
-        public Form1()
+        public frmMainMenu()
         {
             InitializeComponent();
         }
